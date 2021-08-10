5ab69bdbe2fb950ac2cee37d921739c8cd960b68
Patrick Daniel Neary <pneary@umd.edu>
2021-08-09 21:20:54 -0400

    removed secret keys from code lol

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import cred
 
-client_id = '1503a5b603394ab39c408d3d99280437'
-client_secret = '3ed8aa250e054b65aacd511394b7ab63'
-scope = "user-read-recently-played"
 
 sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=cred.client_id, client_secret=cred.client_secret,
                                                redirect_uri=cred.redirect_url, scope=scope))
@@ -14,3 +11,7 @@ results = sp.current_user_recently_played()
 for idx, item in enumerate(results['items']):
     track = item['track']
     print(idx, track['artists'][0]['name'], " – ", track['name'])
+
+
+
+
